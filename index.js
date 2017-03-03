31c55d0a506f792c6aff95423e72a0e1a81d2fc6
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-03-03 10:09:46 +0100

    More cors

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ module.exports = async (request, response) => {
 
   if (!['/', '/companies/view'].includes(pathname)) {
     response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Credentials', 'true')
+    response.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
+    response.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers')
   }
   if (pathname === '/roles') {
     send(response, 200, result)
